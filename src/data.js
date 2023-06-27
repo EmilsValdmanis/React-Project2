8e0d8341cdc7a3115b66b734748297c927f5a0b1
Emīls Valdmanis <valdmanis71@gmail.com>
2023-06-27 20:02:09 +0300

    Added the images and the data

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    image: "library.png",
+    title: "Not Just Library",
+    location: "Taipei",
+    googleMap: "https://g.page/NotJustLibrary?share",
+    description:
+      "This was one of my favorite places to study and just chill out in Taipei. Hidden inside of the Songhan Creative Park, this is a quite and aesthetic space that is filled with art + architecture books and magazines.",
+  },
+  {
+    image: "elephant-mountain.png",
+    title: "Elephant Mountain",
+    location: "Taipei",
+    googleMap: "https://goo.gl/maps/nfnmvniLTVjqUKdu6",
+    description:
+      "This is a highly accessible hike that only takes a half an hour to see the city view. To get to the entrance of the trails, simply take the red line all the way down to the Elephant Mountain MRT station and follow the signs.",
+  },
+  {
+    image: "dadaocheng.png",
+    title: "Dadaocheng",
+    location: "Taipei",
+    googleMap: "https://goo.gl/maps/84uRUckXWtC4f4mZ6",
+    description:
+      "Just a five-min walk away from the Beiman MRT station, this area is home to many fabric shops and indepedent artisan stores. I loved exploring and grabbing snacks from nearby vendors.",
+  },
+];
